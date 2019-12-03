7dd4b9ccacdd297e6c75bdb706ed325261be64e1
Nantipat Suwanvong <scemisnis@gmail.com>
2019-12-03 18:43:19 +0700

    #12 Get all products and display as a table

--- frontend/js/product.js
+++ frontend/js/product.js
@@ -5,16 +5,39 @@ $(function () {
     // Get data when first time open
     getData();
 
-    function getData(){
+    function getData() {
         $("#plist").empty();
         // #12 Get all products and display as a table
         // use $.get
-
+        $.get(`http://localhost:8080/api/products`, function (data, status) {
+            console.log(status);
+            if (status === 'success') {
+                console.log(data);
+                data.forEach(element => {
+                    var serialno = element.serialno;  
+                    var name = element.name,
+                    var category = element.category;
+                    var price = element.price;
+                    var photo = photo;
+                    var item = `<tr>
+                    <td><img src="${photo}" height="30px" alt="picture"></td>
+                    <td>${serialno}</td>
+                    <td>${name}</td>
+                    <td>${category}</td>
+                    <td>${price}</td>
+                    <td><a href="productdetail.html?pid=${data.pid}" class="btn btn-primary" >
+                    View detail
+                    </a></td>
+                    </tr>`;
+                    $("#plist").append(item);
+                });
+            }
+        });
         // ===============================
     }
-    
+
     // Update photo when URL has changed
-    $("#photo").change(function(){
+    $("#photo").change(function () {
         $("#preview").attr("src", $("#photo").val());
     })
 
